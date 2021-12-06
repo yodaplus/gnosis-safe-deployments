391ee999252e89a3137c0414cbc5b6163d0c829f
Taras Yevtushenko <taras.evt@gmail.com>
2021-12-06 12:12:38 +0200

    Add 1.3.1 contracts

--- src/safes.ts
+++ src/safes.ts
@@ -1,25 +1,36 @@
-import GnosisSafeL2130 from './assets/v1.3.0/gnosis_safe_l2.json'
-import GnosisSafe130 from './assets/v1.3.0/gnosis_safe.json'
-import GnosisSafe120 from './assets/v1.2.0/gnosis_safe.json'
-import GnosisSafe111 from './assets/v1.1.1/gnosis_safe.json'
-import GnosisSafe100 from './assets/v1.0.0/gnosis_safe.json'
-import { DeploymentFilter, SingletonDeployment } from './types'
-import { applyFilterDefaults, findDeployment } from './utils'
+import GnosisSafeL2131 from "./assets/v1.3.1/gnosis_safe_l2.json";
+import GnosisSafeL2130 from "./assets/v1.3.0/gnosis_safe_l2.json";
+import GnosisSafe131 from "./assets/v1.3.1/gnosis_safe.json";
+import GnosisSafe130 from "./assets/v1.3.0/gnosis_safe.json";
+import GnosisSafe120 from "./assets/v1.2.0/gnosis_safe.json";
+import GnosisSafe111 from "./assets/v1.1.1/gnosis_safe.json";
+import GnosisSafe100 from "./assets/v1.0.0/gnosis_safe.json";
+import { DeploymentFilter, SingletonDeployment } from "./types";
+import { applyFilterDefaults, findDeployment } from "./utils";
 
 // This is a sorted array (newest to oldest)
 const safeDeployments: SingletonDeployment[] = [
-    GnosisSafe130, GnosisSafe120, GnosisSafe111, GnosisSafe100
-]
+  GnosisSafe131,
+  GnosisSafe130,
+  GnosisSafe120,
+  GnosisSafe111,
+  GnosisSafe100,
+];
 
-export const getSafeSingletonDeployment = (filter?: DeploymentFilter): SingletonDeployment | undefined => {
-    return findDeployment(applyFilterDefaults(filter), safeDeployments)
-}
+export const getSafeSingletonDeployment = (
+  filter?: DeploymentFilter
+): SingletonDeployment | undefined => {
+  return findDeployment(applyFilterDefaults(filter), safeDeployments);
+};
 
 // This is a sorted array (newest to oldest)
 const safeL2Deployments: SingletonDeployment[] = [
-    GnosisSafeL2130
-]
+  GnosisSafeL2131,
+  GnosisSafeL2130,
+];
 
-export const getSafeL2SingletonDeployment = (filter?: DeploymentFilter): SingletonDeployment | undefined => {
-    return findDeployment(applyFilterDefaults(filter), safeL2Deployments)
-}
+export const getSafeL2SingletonDeployment = (
+  filter?: DeploymentFilter
+): SingletonDeployment | undefined => {
+  return findDeployment(applyFilterDefaults(filter), safeL2Deployments);
+};
